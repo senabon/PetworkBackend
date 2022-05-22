d1bfea62a4082713e27516e5e0628759a105dbae
bbkc27 <briana.j.casey@gmail.com>
2022-05-22 12:26:57 -0400

    MongoDB URL added

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const mongoURI = process.env.NODE_ENV === 'production'
 ? process.env.DB_URL
-: 'localhost://4321'
+: process.env.DEV_DB_URL
 
 mongoose.connect(mongoURI)
 .then((instance) => console.log(`Connected to db: ${instance.connections[0].name}`))
