f7cedc78ed8ce76ccb8fca341cb80223df1d23f5
bbkc27 <briana.j.casey@gmail.com>
2022-05-22 13:00:30 -0400

    Port changes

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 require ('dotenv').config()
 
+
+
 const express = require('express')
-
-const app = express()
-
-const mongoose = require('mongoose');
-
 const cors = require('cors')
 const morgan = require('morgan')
 
+const petworkController = require('./controllers/petworkControllers')
+
+const app = express()
+
+
 app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
 
 app.set('port', process.env.port || 4321)
 
