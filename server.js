fc21f7ad60cf715405b7993abea7e09cd94d8728
bbkc27 <briana.j.casey@gmail.com>
2022-05-22 09:19:47 -0400

    Server.js and db/connection.js skeleton complete

---
+++ server.js
@@ -0,0 +1,10 @@
+require ('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+const mongoose = require('mongoose');
+
+const cors = require('cors')
+const morgan = require('morgan')
