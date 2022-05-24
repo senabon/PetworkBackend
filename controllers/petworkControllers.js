b4938e6a426fcd61cc708fd2d27f30c698c06986
senabon <sen.est.diaz@gmail.com>
2022-05-24 16:50:02 -0600

    update route

--- controllers/petworkControllers.js
+++ controllers/petworkControllers.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const Profile = require('../Models/profileSchema')
 const router = express.Router()
 
 //test route
 router.get('/', (req, res) => {
   res.send('welcome to petwork!')
 });
+
+//update 
+router.put('/editprofile', async (req, res) => {
+  try {
+      let updatedProfile = await Profile.findByIdAndUpdate(req.params.id, req.body)
+      console.log(updatedProfile)
+      res.json(updatedProfile)
+  } catch (error) {
+      res.status(4321).json(error);
+  }
+})
 
 module.exports = router;
