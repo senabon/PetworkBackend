f6ab9dd66ec65bff1f56231abab0fd82219413af
senabon <sen.est.diaz@gmail.com>
2022-05-24 16:54:31 -0600

    fixed

--- controllers/petworkControllers.js
+++ controllers/petworkControllers.js
@@ -8,47 +8,4 @@ router.get('/', (req, res) => {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//update 
-router.put('/editprofile', async (req, res) => {
-  try {
-      let updatedProfile = await Profile.findByIdAndUpdate(req.params.id, req.body)
-      console.log(updatedProfile)
-      res.json(updatedProfile)
-  } catch (error) {
-      res.status(4321).json(error);
-  }
-})
-
 module.exports = router;
