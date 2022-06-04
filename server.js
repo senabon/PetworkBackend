6621c8dd1afa3b52e170df2e3cffdd5b33a5ea5c
bbkc27 <briana.j.casey@gmail.com>
2022-06-04 00:47:44 -0400

    Updated CORS info v4

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ const corsOptions ={
   optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header({
+      "Access-Control-Allow-Origin": "https://cheery-salmiakki-f8289b.netlify.app/",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type, *"
+  });
+  next();
+});
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/', petworkController)
