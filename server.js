7a69794823f34cff3aae089c19db095e89bf2c04
bbkc27 <briana.j.casey@gmail.com>
2022-06-04 01:10:58 -0400

    Updated CORS info V5

--- server.js
+++ server.js
@@ -7,26 +7,26 @@ const morgan = require('morgan')
 const petworkController = require('./controllers/petworkControllers')
 
 const app = express()
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 
-const corsOptions ={
-  origin:'https://cheery-salmiakki-f8289b.netlify.app/', 
-  credentials:true,           
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-app.use(function(req, res, next) {
-  res.header({
-      "Access-Control-Allow-Origin": "https://cheery-salmiakki-f8289b.netlify.app/",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-      "Access-Control-Allow-Headers": "Content-Type, *"
-  });
-  next();
-});
+// const corsOptions ={
+//   origin:'https://cheery-salmiakki-f8289b.netlify.app/', 
+//   credentials:true,           
+//   optionSuccessStatus:200
+// }
+app.use(cors());
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/', petworkController)
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+})
 
 app.set(process.env.port || 4321)
 
